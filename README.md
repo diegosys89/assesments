c0633da4471fadd61bef046122048815b592c6d3
Diego Sisalima <diegosys89@gmail.com>
2023-08-29 00:01:00 -0500

    Add service to receiva data via rest post

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # assesments
+How to send a requet api for insert data example
+
+curl -X POST -H "Content-Type: application/json" -d '[{"id": 1234, "job": "test_job"},{"id":9999,"job":"tes2"}]' "http://localhost:4000/insert_data?table_name=jobs"
--- load_batch_data.py
+++ load_batch_data.py
@@ -1,52 +1,24 @@
 import pandas as pd
 from google.cloud import bigquery
+import json
 
 project_id = 'makikuna-integral'
 dataset_id = 'globant_assessment'
 
-data_info = [
-                {
-                    "table_name":"hired_employees",
-                    "path":"hired_employees.csv",
-                    "schema":[
-                            bigquery.SchemaField("id", "INTEGER"),
-                            bigquery.SchemaField("name", "STRING"),
-                            bigquery.SchemaField("datetime", "STRING"),
-                            bigquery.SchemaField("department_id", "INTEGER"),
-                            bigquery.SchemaField("job_id", "INTEGER"),
-                    ]
-                },
-                {
-                    "table_name":"departments",
-                    "path":"departments.csv",
-                    "schema":[
-                            bigquery.SchemaField("id", "INTEGER"),
-                            bigquery.SchemaField("department", "STRING")
-                    ]
-                },
-                {
-                    "table_name":"jobs",
-                    "path":"jobs.csv",
-                    "schema":[
-                            bigquery.SchemaField("id", "INTEGER"),
-                            bigquery.SchemaField("job", "STRING")
-                    ]
-                }
-            ]
+with open("config_data.json") as jc:
+    data_info = json.load(jc)
 
 client = bigquery.Client(project=project_id)
 
-
 for i in data_info:
     # Load CSV data into a Pandas DataFrame
-    df = pd.read_csv(i["path"], names = [n.name for n in i["schema"]])
+    df = pd.read_csv(i["path"], names = [n["name"] for n in i["schema"]])
     # Upload the DataFrame to BigQuery
     table_ref = f"{project_id}.{dataset_id}.{i['table_name']}"
     job_config = bigquery.LoadJobConfig(
-        schema=i['schema'],
+        schema=[bigquery.SchemaField(data_schema["name"],data_schema["type"]) for data_schema in i["schema"]],
         write_disposition="WRITE_TRUNCATE"  # Overwrite table if it exists
     )
-
     job = client.load_table_from_dataframe(df, table_ref, job_config=job_config)
     job.result()  # Wait for the job to complete
     print(f"Loaded {job.output_rows} rows into {table_ref}")
